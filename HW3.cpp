4bb3ecd7c49d138ad82f4a340e36b3376ba832a4
Yeom <76275691+prgmr99@users.noreply.github.com>
2023-05-26 18:25:59 +0900

    Complete

--- HW3.cpp
+++ HW3.cpp
@@ -1,126 +1,165 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int N = 9;
 int Arr[9][4];
 int arr[9];
+int arr2[9];
 int CNT = 0;
-vector<pair<int, int>> p;
-// 회전 수도 페어로 만들면 좋지 않을까?
-// 우선은 회전 수 정보는 제외하고 구현해보자.
+vector<pair<int, int> > p;
+
+struct Card {
+    int top;
+    int right;
+    int bottom;
+    int left;
+    int rotation = 0;
+};
+
+Card cards[9];
+
+void rotate(Card& card) {
+    int temp = card.top;
+    card.top = card.right;
+    card.right = card.bottom;
+    card.bottom = card.left;
+    card.left = temp;
+
+    card.rotation = (card.rotation + 1) % 4;
+}
+
+
+// DFS로 하기 때문에 이전에 생각했던 것처럼 한 번의 경우에 모든 경우를 고려하지 않아도 된다.
+// Rotation을 하는 것도 backtracking
+bool canMatch1(Card& card1, Card& card2) {
+    bool check = false;
+    for(int i=0;i<4;i++) {
+        if((card1.right == p[i].first && card2.left == p[i].second) ||
+        (card1.right == p[i].second && card2.left == p[i].first))
+            check = true;
+    }
+    return check;
+}
+
+bool canMatch2(Card& card1, Card& card2) {
+    bool check = false;
+    for(int i=0;i<4;i++) {
+        if((card1.bottom == p[i].first && card2.top == p[i].second) ||
+        (card1.bottom == p[i].second && card2.top == p[i].first))
+            check = true;
+    }
+    return check;
+}
+
+bool checking(int index) {
+    bool check = false;
+    int k = 0;
+
+    if(index == 0) {
+        return true;
+    }
+    else if(index == 1 || index == 2) {
+        // 왼쪽과 오른쪽의 값들이 pair들의 값과 동일하다.
+        if(canMatch1(cards[arr[index-1]], cards[arr[index]]))
+            check = true;
+    }
+    else if(index == 3 || index == 6) {
+        // 위쪽과 아래쪽의 값들이 pair들의 값과 동일하다.
+        if(canMatch2(cards[arr[index-3]], cards[arr[index]]))
+            check = true;
+    }
+    else if(index == 4 || index == 5 || index == 7 || index == 8) {
+        // 위쪽과 아래쪽의 값들이 pair들의 값과 동일하다.
+        // 왼쪽과 오른쪽의 값들이 pair들의 값과 동일하다.
+        if(canMatch1(cards[arr[index-1]], cards[arr[index]]) &&
+        canMatch2(cards[arr[index-3]], cards[arr[index]]))
+            check = true;
+    }
+    else
+        check = false;
+
+    return check;
+}
+
 
 // promising function
-// 여기서 퍼즐 매칭을 시켜야 해.
-// 지금 k를 넘겨주고 받는 것이 문제가 있는 것 같아.
-// index 변수를 받아야 할 것 같은데
-bool check1(int k) {
-    bool check = true;
-    int flag = 0;
-    
-    for(int i=0;i<4;i++) {
-        for(int j=0;j<4;j++) {
-            if(k == 3 || k == 6) {
-                if((Arr[k][i] == p[i].first && Arr[k-3][j] == p[i].second) ||
-                   (Arr[k][i] == p[i].second && Arr[0][j] == p[i].first)) {
-                    check = true;
-                    break;
-                }
-                else
-                    check = false;
-            }
-            else if(k == 4 || k == 5 || k == 7 || k == 8) {
-                if(((Arr[k][i] == p[i].first && Arr[k-1][j] == p[i].second) ||
-                   (Arr[k][i] == p[i].second && Arr[k-1][j] == p[i].first)) &&
-                   ((Arr[k][i] == p[i].first && Arr[k-3][j] == p[i].second) ||
-                    (Arr[k][i] == p[i].second && Arr[k-3][j] == p[i].first))) {
-                    check = true;
-                    break;
-                }
-                else
-                    check = false;
-            }
-            else if(k == 2) {
-                if(check1(1))
-                    check = true;
-                else
-                    check = false;
-            }
-            else {
-                if((Arr[k][i] == p[i].first && Arr[k+1][j] == p[i].second) ||
-                   (Arr[k][i] == p[i].second && Arr[k+1][j] == p[i].first)) {
-                    check = true;
-                    flag = 1;
-                    break;
-                }
-                else
-                    check = false;
-            }
-        }
-        if(flag == 1)
-            break;
-    }
-    flag = 0;
-    return check;
-}
-
 bool promising(int index) {
-    bool check = true;
-    
     int k = 0;
-    if(index == 0)
-        check = true;
-    
-    // 항상 이전 조건문들을 만족해야하니까
-    while(k<index && check) {
-        if(!check1(k)) {
+    bool check = true;
+
+    while(k < index && check) {
+        // false가 되는 조건을 찾아야 함.
+        // 조건1. 이전에 등장했던 값이면 안된다.
+        // 조건2. 새로운 수를 arr배열에 추가할 때, pair를 이루지 못한다면 안된다.
+        // pair를 이루지 못하는 경우보다 pair를 이루는 경우를 구하고 나머지를 false하는 것이 좋다고 판단
+        // k보다 index를 이제 우선적으로 따져야 하는 이유는 index 이제 막 드려가려고 하는 칸
+        if(arr[k] == arr[index] || !checking(index))
             check = false;
-        }
         k++;
     }
+
+    
     return check;
 }
-// 넘겨받은 index에 따라 false를 정해줘야 해.
 
 // DFS function
-void puzzle(int index) {
-    int j;
-    
-    if(index == N - 1) {
-        // 한 줄로 puzzle 출력하는 부분
+// index는 3x3을 봤을 때, 각 칸을 가리키는 것
+void DFS(int index) {
+    // 끝에 도달했을 경우
+    if(index == 9) {
         CNT++;
-        for(int i = 0;i < N;i++)
-            cout << arr[i] << " ";
+        for(int i=0;i<9;i++) {
+            cout << arr[i] + 1 << "(" << arr2[i] << ")" << " ";
+        }
         cout << '\n';
+        return;
     }
-    
-    // 교재랑 다른 부분
-    for(j=0;j<9;j++) {
-        arr[index] = j+1;
-        if(promising(index))
-            puzzle(index+1);
+
+    // 이런 식으로 가지를 만드는 것이 가장 적합한 것 같음.
+    for(int i=0;i<9;i++) {
+        for(int j=0;j<4;j++) {
+            arr[index] = i;
+            arr2[index] = cards[arr[index]].rotation;
+            if(promising(index))
+                DFS(index+1);
+            rotate(cards[arr[index]]);
+        }
     }
 }
 
 
-// Main function
 int main(void) {
+    // 짝을 pair를 통해서 구현
     p.push_back(make_pair(1,2));
     p.push_back(make_pair(3,4));
     p.push_back(make_pair(5,6));
     p.push_back(make_pair(7,8));
-    
-    
+
+    // 1부터 9까지의 카드를 입력
     for(int i=0;i<9;i++) {
         for(int j=0;j<4;j++) {
             cin >> Arr[i][j];
         }
     }
     
-    // 각각의 index에 대해서 puzzle 함수를 돌려야 해
-    puzzle(0);
-    // 경우의 수 출력
+    // 구조체 배열 초기화
+    for(int i=0;i<9;i++) {
+        cards[i].top = Arr[i][0];
+        cards[i].right = Arr[i][1];
+        cards[i].bottom = Arr[i][2];
+        cards[i].left = Arr[i][3];
+        cards[i].rotation = 0;
+    }
+
+    // 카드 조합찾기 시작
+    DFS(0);
+
+    // 조합의 경우의 수 출력
     cout << CNT << '\n';
+
     return 0;
 }
+
+
